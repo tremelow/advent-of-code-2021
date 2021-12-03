2323e2121d05dad7861b16f5d6450236d700b5b4
tremelow <29092877+tremelow@users.noreply.github.com>
2021-12-03 15:56:02 +0100

    cleanup day2

--- day2/main.jl
+++ day2/main.jl
@@ -5,21 +5,13 @@ data = readdlm("day2/input.txt", ' ')
 strDirections = String.( data[:,1] )
 speed = Int.( data[:,2] )
 
-
 ##########################################
 ##  Question 1
+vTranslate = Dict("up" => -1, "down" => 1, "forward" => 0)
+vMoves = replace(strDirections, vTranslate...) .* speed
+hMoves = (strDirections .== "forward") .* speed
 
-# Moves
-translateDirections = Dict(
-    "forward"   => [1, 0],
-    "up"        => [0, -1],
-    "down"      => [0, 1]
-)
-cardDirections = replace(strDirections, translateDirections...)
-moves = cardDirections .* speed
-
-# Final position
-result = prod(sum(moves))
+result = sum(vMoves) * sum(hMoves)
 println("Produit des coordonnées finales, méthode 1 : $result")
 
 
@@ -28,9 +20,8 @@ println("Produit des coordonnées finales, méthode 1 : $result")
 translateAim = Dict("up" => -1, "down" => 1, "forward" => 0)
 aimChanges = replace(strDirections, translateAim...) .* speed
 aim = cumsum(aimChanges)
-hChanges = (aimChanges .== 0) .* speed
-vChanges = (aimChanges .== 0) .* aim .* speed
+hChanges = (strDirections .== "forward") .* speed
+vChanges = hChanges .* aim
 
-# Final position
 result = sum(hChanges) * sum(vChanges)
 println("Produit des coordonnées finales, méthode 2 : $result")
