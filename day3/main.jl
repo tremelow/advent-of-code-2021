045d4b7a5184a297babf7c79bc79e627195eb5c1
tremelow <29092877+tremelow@users.noreply.github.com>
2021-12-03 15:50:38 +0100

    clean-up

--- day3/main.jl
+++ day3/main.jl
@@ -1,28 +1,18 @@
 using DelimitedFiles
 
 data = readdlm("day3/input.txt", String)[:]
-strBits = hcat(split.(data, "")...) 
-bits = (strBits .== "1")
-
+bits = ( hcat(split.(data, "")...) .== "1" )
 bitSize, N = size(bits)
-nbOnes = sum(bits, dims=2)
 
-gammaBits   = (nbOnes .> N/2)
-epsilonBits = broadcast(!, gammaBits)
-
+bitsToInt = arr ->  reverse!(arr[:]).chunks[1]
 
 ##############################################
 ##  Question 1
-function bitsToInt(arr)
-    # Peu efficient mais code compact uwu
-    str = "0b" * prod(broadcast(b -> b ? "1" : "0", arr))
-    parse(Int, str)
-end
+gammaBits   = (sum(bits, dims=2) .> N/2)
+epsilonBits = broadcast(!, gammaBits)
+gamma, epsilon = bitsToInt(gammaBits), bitsToInt(epsilonBits)
 
-γ = bitsToInt(gammaBits)
-ε = bitsToInt(epsilonBits)
-
-result = γ * ε
+result = gamma * epsilon
 println("Produit des nombres γ et ε : $result")
 
 
@@ -31,21 +21,20 @@ println("Produit des nombres γ et ε : $result")
 ##  Question 2
 
 function getIndices(comp)
-    iBit = 1
-    validIndices = trues(N)
+    iBit, validIndices = 1, trues(N)
     while sum(validIndices) > 1
-        newN = sum(validIndices)
         # bit de comparaison
-        compBit = comp.( sum(bits[iBit, validIndices]) , newN/2 )
-        validIndices .&= ( bits[iBit, :] .== compBit )
+        compBit = comp.(
+            sum(bits[iBit, validIndices]) , sum(validIndices)/2 )
+        # filtrage des bits invalides
+        @. validIndices &= ( bits[iBit, :] == compBit )
         iBit += 1
     end
-
     validIndices
 end
 
-
 oxBits, coBits = bits[:, getIndices(>=)], bits[:, getIndices(<)]
 oxNumber, coNumber = bitsToInt(oxBits), bitsToInt(coBits)
+
 result = oxNumber * coNumber
 println("Life support rating : $result")
