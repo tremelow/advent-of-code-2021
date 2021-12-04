70ad136cf025130eb85fad3b15e7d6c3954b0acf
tremelow <29092877+tremelow@users.noreply.github.com>
2021-12-04 10:59:49 +0100

    day 4
    Finalement j'ai pas utilisé d'expressions, juste des fonctions

---
+++ day4/main.jl
@@ -0,0 +1,48 @@
+using DelimitedFiles
+
+#####################
+##  Parse data
+concatCards, brokenHeader = readdlm("day4/input.txt", Int, header=true)
+
+nb = parse.( Int, split(brokenHeader[1], ',') )
+nextNb = Iterators.Stateful(nb)
+nRow, nCol = 5, 5
+cards = permutedims( reshape(concatCards, (nRow,:,nCol)), [1,3,2] )
+
+
+#############################
+##  Utilities 
+isDrawn = falses(size(cards))
+
+# Find winner(s) on the fly
+hasWinRow() = any( all(isDrawn, dims=2), dims=1 )[:]
+hasWinCol() = any( all(isDrawn, dims=1), dims=2 )[:]
+flagWins()  = hasWinRow() .| hasWinCol()
+
+drawNext() = popfirst!(nextNb) |> n -> (@. isDrawn |= (cards == n))
+computeScore(idx, lastDrawn) = 
+    sum(cards[:,:,idx] .* map(!,isDrawn[:,:,idx]) ) * lastDrawn
+
+
+################################
+##  Question 1
+while !any( flagWins() )
+    drawNext()
+end
+
+winnerBoard = findfirst( flagWins() )
+result = computeScore(winnerBoard, nb[nextNb.taken])
+println("Le score du bingo gagnant est $result")
+
+##############################
+## Question 2
+while sum(1 .- flagWins() ) > 1
+    drawNext()
+end
+loserBoard = findfirst( map(!, flagWins()) )
+while !all( flagWins() )
+    drawNext()
+end
+
+result = computeScore(loserBoard, nb[nextNb.taken])
+println("Le score du bingo qui gagne le dernier est $result")
