86d13103be19727bfe69606f4dde03d3bc400b26
tremelow <29092877+tremelow@users.noreply.github.com>
2021-12-05 14:27:15 +0100

    slight cleanup

--- day5/main.jl
+++ day5/main.jl
@@ -1,18 +1,13 @@
 using DelimitedFiles
 using OffsetArrays
-using LinearAlgebra: diag
-using Base.Iterators: product
-
-data = readdlm("day5/input.txt", String)
-
 
 ## Parsing
-function toTuple(strVec)
-    intMat = parse.( Int, hcat( split.(strVec, ',')... ) )
+function toTuple(vecCoords)
+    intMat = parse.( Int, hcat( split.(vecCoords, ',')... ) )
     Tuple.( reverse.( eachcol(intMat) ) )
 end
-fstIndices = toTuple(data[:,1])
-scdIndices = toTuple(data[:,3])
+data = readdlm("day5/input.txt", String)
+fstIndices, scdIndices = toTuple(data[:,1]), toTuple(data[:,3])
 
 # Create iterators from index1 to index2 (for each coordinate)
 function fromTo(idx1, idx2)
@@ -21,9 +16,8 @@ function fromTo(idx1, idx2)
 end
 
 # Initialise danger matrix, with indices starting at 0
-southEast = maximum( CartesianIndex.( vcat(fstIndices,scdIndices) ) )
-nRow, nCol = southEast[1]+1, southEast[2]+1
-dangerRating = OffsetMatrix( zeros(Int, nRow, nCol), -1, -1)
+lastIdx = maximum( CartesianIndex.( vcat(fstIndices,scdIndices) ) )
+dangerRating = OffsetMatrix( zeros(Int, Tuple(lastIdx)), -1, -1)
 
 
 ####################################
@@ -41,12 +35,13 @@ println("Le nombre de zones dangereuses est $result")
 
 ##################################
 ## Question 2
-isSqr(idx1, idx2) = ( abs(idx1[1] - idx2[1]) == abs(idx1[2] - idx2[2]) )
+isSquare(idx1, idx2) = ( abs(idx1[1] - idx2[1]) == abs(idx1[2] - idx2[2]) )
 for (idx1,idx2) in zip(fstIndices, scdIndices)
-    if isSqr(idx1, idx2)
+    if isSquare(idx1, idx2)
         toVisit = CartesianIndex.( zip(fromTo(idx1,idx2)...) )
         dangerRating[toVisit] .+= 1
     end
 end
+
 result = sum( dangerRating .> 1 )
 println("Oups avec les diagonales en fait c'est $result")
